85b486b5803489745f78f95c433a04228e9d04cc
rabkinda <rabkinda@mail.tau.ac.il>
2018-10-24 21:20:00 +0300

    print

--- network/dataloader.py
+++ network/dataloader.py
@@ -8,21 +8,23 @@ import os
 
 
 class Dataset(torchdata.Dataset):
-    def __init__(self, is_train):
+    def __init__(self, is_train, txt_path):
+        video_names_to_filter = [x.rstrip() for x in open(txt_path, 'r')]
         descriptor_dir = '../pose_estimations'
         descriptor_files = [vid_dsc_file for vid_dsc_file in glob.glob(descriptor_dir + "/*.pickle")]
 
         self.objects = []
 
         for descriptor_file in descriptor_files:
-            with (open(descriptor_file, "rb")) as file:
-                ndarray_dict = pickle.load(file)
-                video_dsc = np.zeros((len(ndarray_dict), 2, 17, 2, 2))
+            if any(vid_filter for vid_filter in video_names_to_filter if vid_filter in descriptor_file):
+                with (open(descriptor_file, "rb")) as file:
+                    ndarray_dict = pickle.load(file)
+                    video_dsc = np.zeros((len(ndarray_dict), 2, 17, 2, 2))
 
-                for key in range(0, len(ndarray_dict)):
-                    video_dsc[key] = ndarray_dict[str(key)]
+                    for key in range(0, len(ndarray_dict)):
+                        video_dsc[key] = ndarray_dict[str(key)]
 
-                self.objects.append((torch.from_numpy(video_dsc), torch.from_numpy(self.getLabelFromFilename(descriptor_file))))
+                    self.objects.append((torch.from_numpy(video_dsc), self.getLabelFromFilename(descriptor_file)))
 
         if is_train:
             random.shuffle(self.objects)
@@ -41,12 +43,12 @@ class Dataset(torchdata.Dataset):
     def getLabelFromFilename(self, descriptor_file):
         filename = os.path.splitext(os.path.basename(descriptor_file))[0]
         result_letter = filename.split('-')[-1]
-        return self.get_result_one_hot_vector(result_letter)
+        return self.get_label_from_letter(result_letter)
 
 
-    def get_result_one_hot_vector(self, letter):
+    def get_label_from_letter(self, letter):
         return {
-            'L': np.array([1, 0, 0]),
-            'R': np.array([0, 1, 0]),
-            'T': np.array([0, 0, 1])
+            'L': 0,
+            'R': 1,
+            'T': 2
         }.get(letter)
