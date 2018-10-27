faa1d6ca5dc2c7cddf6262b59eb9049ad014aa05
rabkinda <rabkinda@mail.tau.ac.il>
2018-10-27 15:28:41 +0300

    optimization of digit recognizer by using correlation to static digit images

--- DigitRecognizer.py
+++ DigitRecognizer.py
@@ -3,21 +3,7 @@ import numpy as np
 import pyocr
 import pyocr.builders
 from PIL import Image
-
-
-def getDigit(digitImgArr):
-    img = Image.fromarray(digitImgArr)
-    tools = pyocr.get_available_tools()[0]
-
-    for i in range(6, 10 + 1):
-        text = tools.image_to_string(process(img), builder=pyocr.builders.DigitBuilder(i))
-        if text.isdecimal() and 0 <= int(text) and int(text) <= 15:
-            break
-
-    if not text.isdecimal():
-        return getDigitByImgDiff(digitImgArr)
-
-    return int(text)
+from scipy.signal import correlate2d
 
 
 def process(img):
@@ -29,20 +15,38 @@ def process(img):
     return img
 
 
-def getDigitByImgDiff(digitImgArr):
+static_digits_map = {}
 
+for i in range(16):
+    curr_fixed_img = Image.open('digit_images/' + str(i) + '.png')
+    curr_processed_fixed_img = process(curr_fixed_img)
+    static_digits_map[str(i)] = curr_processed_fixed_img
+
+
+def getDigit(digitImgArr):
+    # img = Image.fromarray(digitImgArr)
+    # tools = pyocr.get_available_tools()[0]
+    #
+    # for i in range(6, 10 + 1):
+    #     text = tools.image_to_string(process(img), builder=pyocr.builders.DigitBuilder(i))
+    #     if text.isdecimal() and 0 <= int(text) and int(text) <= 15:
+    #         break
+    #
+    # if not text.isdecimal():
+    #     return getDigitByImgDiff(digitImgArr)
+    #
+    # return int(text)
+    return getDigitByImgDiff(digitImgArr)
+
+
+def getDigitByImgDiff(digitImgArr):
     img = Image.fromarray(digitImgArr)
     processed_img = process(img)
-    diffArr = np.zeros(16)
+    processed_img = np.pad(processed_img, [2,2], mode='edge')
 
-    for i in range(0, 16):
-        curr_fixed_img = Image.open('digit_images/'+str(i)+'.png')
-        curr_processed_fixed_img = process(curr_fixed_img)
-        diffArr[i] = np.sum(np.asarray(processed_img)-np.asarray(curr_processed_fixed_img))/(np.sum(np.asarray(processed_img))+np.sum(np.asarray(curr_processed_fixed_img)))
-
-    return int(np.argmin(diffArr))
-
-
-# digitImgArr = np.asarray(Image.open('digit_images/15.png'))
-# res=getDigitByImgDiff(digitImgArr)
-# print(str(res))
+    digit = np.argmax([np.max(correlate2d(np.subtract(static_digits_map[str(i)], np.mean(static_digits_map[str(i)])),
+                               np.subtract(processed_img, np.mean(processed_img)),
+                               boundary='fill',
+                               mode='valid',
+                               fillvalue=255)) for i in range(16)])
+    return digit
