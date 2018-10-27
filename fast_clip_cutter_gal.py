e225519a906472bbc1b3d79944a547c5782015d0
rabkinda <rabkinda@mail.tau.ac.il>
2018-10-27 23:13:08 +0300

    better error handling of cutter script

--- fast_clip_cutter_gal.py
+++ fast_clip_cutter_gal.py
@@ -12,16 +12,22 @@ from VideoUtils import CV2VideoCapture, ffmpeg_extract_subclip, find_hit_info
 videos_to_cut = glob.glob(os.getcwd() + "/precut/" + "*.mp4").__len__()
 print("Cutting", videos_to_cut, "videos")
 
-#already_processed_videos = [os.path.splitext(ntpath.basename(vid))[0] for vid in glob.glob(os.getcwd() + "/videos/" + "*.mp4")]
+success_file_read = open("cutter_success.txt", "r")
+already_processed_videos = [x.rstrip() for x in success_file_read]
+success_file_read.close()
+
+success_file = open("cutter_success.txt", "a")
+error_file = open("cutter_error.txt", "w")
+
 video_num = 0
 
 for vid in glob.glob(os.getcwd() + "/precut/" + "*.mp4"):
-    #videoName = os.path.splitext(ntpath.basename(vid))[0]
+    videoName = os.path.splitext(ntpath.basename(vid))[0]
     video_num += 1
     print('processing video '+ str(video_num))
 
-    #if np.sum([videoName in s for s in already_processed_videos])>15 or videoName in ['_wkJXBxOsiE']:
-    #    continue
+    if videoName in already_processed_videos:
+        continue
 
     print("Video:", vid)
     clips_recorded = 0
@@ -30,17 +36,26 @@ for vid in glob.glob(os.getcwd() + "/precut/" + "*.mp4"):
     fps = cap.get_fps()
     print("Length of Vid:", cap.__len__())
 
-    while cap.get_position() <= cap.__len__():
-        print(cap.get_position(), "big while loop", cap.__len__())
-        hit_pos, next_clip_start_pos, label = find_hit_info(cap)
-        if hit_pos == -1:
-            break
+    try:
+        while cap.get_position() <= cap.__len__():
+            print(cap.get_position(), "big while loop", cap.__len__())
+            hit_pos, next_clip_start_pos, label = find_hit_info(cap)
+            if hit_pos == -1:
+                break
 
-        if hit_pos - 50>=0:
-            targetname = 'videos/' + Path(vid).stem + "-" + str(clips_recorded) + "-" + label + "-" + str(hit_pos) + '.mp4'
-            ffmpeg_extract_subclip(vid, t1=(hit_pos - 50)/fps, n_frames=60, targetname=targetname)
-            clips_recorded += 1
+            if hit_pos - 50>=0:
+                targetname = 'videos/' + Path(vid).stem + "-" + str(clips_recorded) + "-" + label + "-" + str(hit_pos) + '.mp4'
+                ffmpeg_extract_subclip(vid, t1=(hit_pos - 50)/fps, n_frames=60, targetname=targetname)
+                clips_recorded += 1
 
-        if next_clip_start_pos == -1:
-            break
-        cap.set_position(next_clip_start_pos)
+            if next_clip_start_pos == -1:
+                break
+            cap.set_position(next_clip_start_pos)
+
+        success_file.write(videoName + '\n')
+    except:
+        error_file.write(videoName + '\n')
+
+success_file.close()
+error_file.close()
+print('Done!!')
