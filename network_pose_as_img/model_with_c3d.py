d7f2e3636d22ba2ebb235ba43ef001e62c572b25
rabkinda <rabkinda@mail.tau.ac.il>
2018-11-20 17:58:49 +0200

    c3d model small modifications

--- network_pose_as_img/model_with_c3d.py
+++ network_pose_as_img/model_with_c3d.py
@@ -1,6 +1,4 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class C3D(nn.Module):
@@ -8,23 +6,24 @@ class C3D(nn.Module):
     def __init__(self):
         super(C3D, self).__init__()
 
-        self.conv1 = nn.Conv3d(1, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        x = 32
+        self.conv1 = nn.Conv3d(1, x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
-        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv2 = nn.Conv3d(x, 2*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv3a = nn.Conv3d(2*x, 4*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv3b = nn.Conv3d(4*x, 4*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4a = nn.Conv3d(4*x, 8*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4b = nn.Conv3d(8*x, 8*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+        self.conv5a = nn.Conv3d(8*x, 8*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv5b = nn.Conv3d(8*x, 8*x, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 0, 0))
 
         self.fc6 = nn.Linear(8192, 4096)
         self.fc7 = nn.Linear(4096, 4096)
@@ -38,48 +37,31 @@ class C3D(nn.Module):
 
     def forward(self, x):
 
-        print('\nstart:       ' + str(x.shape))#[1, 64, 16, 128, 256]
         h = self.relu(self.conv1(x))
-        print('after conv1: '+str(h.shape))#[1, 64, 16, 64, 128]
         h = self.pool1(h)
-        print('after pool1: '+str(h.shape))#[1, 128, 16, 64, 128]
 
         h = self.relu(self.conv2(h))
-        print('after conv2: '+str(h.shape))#[1, 128, 8, 32, 64]
         h = self.pool2(h)
-        print('after pool2: '+str(h.shape))#[1, 256, 8, 32, 64]
 
         h = self.relu(self.conv3a(h))
-        print('after conv3a: '+str(h.shape))#[1, 256, 8, 32, 64]
         h = self.relu(self.conv3b(h))
-        print('after conv3b: '+str(h.shape))#[1, 256, 4, 16, 32]
         h = self.pool3(h)
-        print('after pool3: '+str(h.shape))#[1, 512, 4, 16, 32]
 
         h = self.relu(self.conv4a(h))
-        print('after conv4a: '+str(h.shape))#[1, 512, 4, 16, 32]
         h = self.relu(self.conv4b(h))
-        print('after conv4b: '+str(h.shape))#[1, 512, 2, 8, 16]
         h = self.pool4(h)
-        print(h.shape)
 
         h = self.relu(self.conv5a(h))
-        print(h.shape)
         h = self.relu(self.conv5b(h))
-        print(h.shape)
         h = self.pool5(h)
-        print(h.shape)
 
         h = h.view(-1, 8192)
         h = self.relu(self.fc6(h))
-        print(h.shape)
         h = self.dropout(h)
         h = self.relu(self.fc7(h))
-        print(h.shape)
         h = self.dropout(h)
 
         logits = self.fc8(h)
-        print(h.shape)
         probs = self.log_softmax(logits)
 
         return probs
@@ -94,8 +76,8 @@ class FencingModel(nn.Module):
 
     def forward(self, frames_pose_tensor):
         seq_len = frames_pose_tensor.shape[1]
-        seqs_to_count = [i for i in range(seq_len) if ((i>=25 and i < 40 and i % 3 == 0) or (i >= 40 and i < 51))]
+        seqs_to_count = [i for i in range(seq_len) if (i>=0 and i<=50 and i%2==0)]
 
-        input = frames_pose_tensor[:, seqs_to_count].unsqueeze(1)#(N, C_{in}, D_{in}, H_{in}, W_{in})
+        input = frames_pose_tensor[:, seqs_to_count[-16:]].unsqueeze(1)#(N, C_{in}, D_{in}, H_{in}, W_{in})
         res = self.c3d(input)
         return res
