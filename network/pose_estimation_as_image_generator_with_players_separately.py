9eb4c6e669bb479ec9e124e399b3e476eb5c44cc
rabkinda <rabkinda@mail.tau.ac.il>
2018-11-22 15:14:01 +0200

    cropping y-axis approximately between y_min and y_max of players for each clip profiling changes in code

--- network/pose_estimation_as_image_generator_with_players_separately.py
+++ network/pose_estimation_as_image_generator_with_players_separately.py
@@ -17,7 +17,7 @@ def getPoseEstimationImgFromCoordinatesByArr(oriImg, coords_arr):
 
 
 mode = 'train'
-output_dir = '/home/rabkinda/Documents/computer_vision/fencing/poses_clips_reduced_players_different_channel/'+mode
+output_dir = '/home/rabkinda/Documents/computer_vision/fencing/poses_clips_reduced_players_different_channel1/' + mode
 clips_path = '/media/rabkinda/Gal_Backup/fencing/clips*/*.mp4'
 
 clip_paths = [f for f in glob.glob(clips_path) if 'None' not in f]
@@ -32,41 +32,60 @@ if pickle_file.is_file():
 
 max_parallel = 8
 pool = Pool(max_parallel)
+padding = 33
+
 
 def generate_poses_clip(obj):
-
     curr_poses = obj[0]
     curr_poses = curr_poses.numpy()
 
-    curr_clip_name = obj[2]
-    clip_path = [f for f in clip_paths if curr_clip_name in f][0]
-    cap = CV2VideoCapture(clip_path)
+    all_y = curr_poses[:, :, :, :, 1]
+    y_max = np.max(all_y[all_y > 0])
+    y_min = np.min(all_y[all_y > 0])
+    assert not np.isinf(y_max)
+    assert not np.isinf(y_min)
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(os.path.join(output_dir, curr_clip_name) + '.mp4', fourcc, 20.0, img_shape)
+    curr_clip_name = obj[2]
+    # clip_path = [f for f in clip_paths if curr_clip_name in f][0]
+    # cap = CV2VideoCapture(clip_path)
+
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    # out = cv2.VideoWriter(os.path.join(output_dir, curr_clip_name) + '.mp4', fourcc, 20.0, img_shape)
 
     for seq_ind in range(seq_len):
         curr_fencing_players_coords = curr_poses[seq_ind]
-        curr_frame_img = cap.read()
+        # curr_frame_img = cap.read()
 
         for p in range(len(curr_fencing_players_coords)):
+            curr_frame_img = np.zeros((720 // 2, 1280 // 5, 3), dtype=np.uint8)
+            curr_fencing_players_coords[p, :, :, 0] = curr_fencing_players_coords[p, :, :, 0] / 5.0
+            curr_fencing_players_coords[p, :, :, 1] = curr_fencing_players_coords[p, :, :, 1] / 2.0
+
             curr_frame_with_chosen_pose = getPoseEstimationImgFromCoordinatesByArr(curr_frame_img,
-                                                                                    np.expand_dims(curr_fencing_players_coords[p], axis=0))
+                                                                                   np.expand_dims(
+                                                                                       curr_fencing_players_coords[p],
+                                                                                       axis=0))
 
-            pose_only_as_img = curr_frame_with_chosen_pose.astype(np.float32) - curr_frame_img.astype(np.float32)
-            pose_only_as_img = np.clip(pose_only_as_img, 0, 255)
+            # pose_only_as_img = curr_frame_with_chosen_pose.astype(np.float32) - curr_frame_img.astype(np.float32)
+            # pose_only_as_img = np.clip(pose_only_as_img, 0, 255)
 
-            #poses_only_as_img_gray = cv2.cvtColor(poses_only_as_img, cv2.COLOR_RGB2GRAY)
-            #ret, pose_only_as_img = cv2.threshold(pose_only_as_img, 0, 255, cv2.THRESH_BINARY)
-            #poses_only_as_img_rgb = cv2.cvtColor(poses_only_as_img_gray, cv2.COLOR_GRAY2RGB)
+            # poses_only_as_img_gray = cv2.cvtColor(poses_only_as_img, cv2.COLOR_RGB2GRAY)
+            # ret, pose_only_as_img = cv2.threshold(pose_only_as_img, 0, 255, cv2.THRESH_BINARY)
+            # poses_only_as_img_rgb = cv2.cvtColor(poses_only_as_img_gray, cv2.COLOR_GRAY2RGB)
 
-            img = Image.fromarray(pose_only_as_img.astype(np.uint8))
-            img = img.resize(img_shape, Image.ANTIALIAS)
+            img = Image.fromarray(
+                curr_frame_with_chosen_pose[max(0, int(y_min / 2) - padding):min(720 // 2,int(y_max / 2) + padding), :, :])
+            img = img.resize(img_shape, Image.ANTIALIAS)  # ANTIALIAS
 
-            out.write(np.array(img))
+            curr_output_dir = os.path.join(output_dir, curr_clip_name, str(p))
+            if not os.path.exists(curr_output_dir):
+                os.makedirs(curr_output_dir)
+            curr_output_path = os.path.join(curr_output_dir, str(seq_ind) + '.png')
+            img.save(curr_output_path)
+            # out.write(np.array(img))
 
-    out.release()
-    cap.__del__()
+    # out.release()
+    # cap.__del__()
     cv2.destroyAllWindows()
 
 
