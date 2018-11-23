b5262196c12b7f93b693cf716f884870403b8f12
rabkinda <rabkinda@mail.tau.ac.il>
2018-11-23 16:53:13 +0200

    model with 3 lstms

--- network_pose_as_img/model_with_triplet_conv_lstm.py
+++ network_pose_as_img/model_with_triplet_conv_lstm.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from network_pose_as_img.convlstm import ConvLSTM
 
 
@@ -14,7 +13,7 @@ class Encoder(nn.Module):
         self.layers = torch.nn.Sequential()
 
         # encoder_1: [batch, 128, 256, in_channels] => [batch, 64, 128, ngf]
-        self.layers.add_module(name='conv2d0', module=torch.nn.Conv2d(kernel_size=4, stride=2, in_channels=1, out_channels=ngf, padding=1))
+        self.layers.add_module(name='conv2d0', module=torch.nn.Conv2d(kernel_size=4, stride=2, in_channels=3, out_channels=ngf, padding=1))
 
         layer_specs = [
             ngf * 2,  # encoder_2: [batch, 64, 128, ngf] => [batch, 32, 64, ngf * 2]
@@ -38,7 +37,7 @@ class Encoder(nn.Module):
             layer_ind+=1
 
     def forward(self, pose_as_img):
-        res = self.layers(pose_as_img.unsqueeze(1))
+        res = self.layers(pose_as_img)
         return res
 
 
@@ -50,36 +49,66 @@ class FencingModel(nn.Module):
 
         self.encoder = Encoder()
 
-        self.rnn = ConvLSTM(input_size=(8, 16),
-                         input_dim=32*8,
-                         hidden_dim=[64],
-                         kernel_size=(3, 3),
-                         num_layers=1,
-                         batch_first=True,
-                         bias=True,
-                         return_all_layers=False)
+        self.rnn_left_player = ConvLSTM(input_size=(8, 16),
+                                     input_dim=32 * 8,#256
+                                     hidden_dim=[64],
+                                     kernel_size=(3, 3),
+                                     num_layers=1,
+                                     batch_first=True,
+                                     bias=True,
+                                     return_all_layers=False)
 
-        self.fc1 = nn.Linear(8192, 2048)
-        self.fc2 = nn.Linear(2048, 512)
-        self.fc3 = nn.Linear(512, output_size)
+        self.rnn_right_player = ConvLSTM(input_size=(8, 16),
+                                        input_dim=32 * 8,#256
+                                        hidden_dim=[64],
+                                        kernel_size=(3, 3),
+                                        num_layers=1,
+                                        batch_first=True,
+                                        bias=True,
+                                        return_all_layers=False)
+
+        self.rnn_overall = ConvLSTM(input_size=(8, 16),
+                                         input_dim=64*2,
+                                         hidden_dim=[64],
+                                         kernel_size=(3, 3),
+                                         num_layers=1,
+                                         batch_first=True,
+                                         bias=True,
+                                         return_all_layers=False)
+
+        self.fc1 = nn.Linear(8192, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, output_size)
 
         self.dropout = nn.Dropout(p=0.5)
         self.relu = nn.ReLU()
 
 
     def forward(self, frames_pose_tensor):
-        seq_len = frames_pose_tensor.shape[1]
-        batch_size = frames_pose_tensor.shape[0]
-        encoding = []
-        seqs_to_count = [i for i in range(seq_len) if ((i>=25 and i < 40 and i % 3 == 0) or (i >= 40 and i < 51))]
+        batch_size, seq_len, people_num, img_channel_size, h, w = frames_pose_tensor.shape
+        seqs_to_count = [i for i in range(seq_len) if (i >= 0 and i <= 52 and i % 2 == 0)]
+        seqs_to_count = seqs_to_count[-16:]
+        encodings_player0 = []
+        encodings_player1 = []
 
         for seq_ind in seqs_to_count:
-            encoding.append(self.encoder(frames_pose_tensor[:, seq_ind]))
+            for p in range(people_num):
+                curr_encoding = self.encoder(frames_pose_tensor[:, seq_ind, p])
+                if p==0:
+                    encodings_player0.append(curr_encoding)
+                else:
+                    encodings_player1.append(curr_encoding)
 
-        clip_encodings = torch.stack(encoding).transpose(0,1)
-        outputs, state = self.rnn(clip_encodings)
+        encodings_player0 = torch.stack(encodings_player0).transpose(0, 1)
+        encodings_player1 = torch.stack(encodings_player1).transpose(0, 1)
+        outputs_player0, state_player0 = self.rnn_left_player(encodings_player0)
+        outputs_player1, state_player1 = self.rnn_right_player(encodings_player1)
 
-        last_output = outputs[0][:, -1].view(batch_size, -1)
+        players_1_stage_outputs_combined = torch.cat([outputs_player0[0], outputs_player1[0]], dim=2)
+        outputs_overall, state_overall = self.rnn_overall(players_1_stage_outputs_combined)
+
+        #maybe add decoder here
+        last_output = outputs_overall[0][:, -1].view(batch_size, -1)
         res = self.relu(self.fc1(self.dropout(last_output)))
         res = self.relu(self.fc2(self.dropout(res)))
         res = self.fc3(self.dropout(res))
