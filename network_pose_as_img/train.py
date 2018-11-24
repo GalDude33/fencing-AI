d2728f4c63c94fa76679f770f40116148e78f3d2
rabkinda <rabkinda@mail.tau.ac.il>
2018-11-24 20:23:45 +0200

    added arguments and results writing to file

--- network_pose_as_img/train.py
+++ network_pose_as_img/train.py
@@ -11,19 +11,29 @@ import numpy as np
 from network_pose_as_img.model_with_c3d import FencingModel
 from network_pose_as_img.dataloader2 import Dataset
 from network.utils import AverageMeter, BinCounterMeter, adjust_learning_rate, accuracy, check_grad
+import argparse
+import sys
 
 
-filtered_seq_len = 32
-filtered_seq_step_size = 1
-use_optical_flow = 1
-batch_size = 14
-workers = 18
+parser = argparse.ArgumentParser()
+# Env options:
+parser.add_argument('--filtered_seq_len', type=int, default=24, help='filtered_seq_len')
+parser.add_argument('--filtered_seq_step_size', type=int, default=1, help='filtered_seq_step_size')
+parser.add_argument('--use_optical_flow', type=int, default=1, help='use_optical_flow')
+parser.add_argument('--batch_size', type=int, default=16, help='batch_size')
+parser.add_argument('--workers', type=int, default=16, help='workers')
+parser.add_argument('--learning_rate', type=float, default=1e-5, help='learning_rate')
+parser.add_argument('--epochs', type=int, default=100, help='epochs')
+parser.add_argument('--poses_video_path', type=str, help='poses_video_path')
+parser.add_argument('--base_exp_name', type=str, help='base_exp_name')
+
+args = parser.parse_args()
 use_cuda = True
-learning_rate = 1e-5
 weight_decay = 0#1e-5
 checkpoint = ''
-expName = 'fencing_exp_poses_as_img_c3d_{lr}_wd_0_b_{batch}_look_{seq_len}_step{step}_{date}'.format(lr=learning_rate, batch=batch_size, seq_len=filtered_seq_len, step=filtered_seq_step_size, date=datetime.now())
-epochs = 100
+expName = '{base_exp_name}_lr_{lr}_wd_0_b_{batch}_look_{seq_len}_step_{step}_{date}'\
+    .format(base_exp_name=args.base_exp_name, lr=args.learning_rate, batch=args.batch_size, seq_len=args.filtered_seq_len,
+            step=args.filtered_seq_step_size, date=datetime.now())
 adjust_lr_manually = 1
 max_not_improving_epochs = 10
 clip_grad = 0.5
@@ -32,27 +42,25 @@ labels_arr = np.array([0, 1, 2])
 device = torch.device("cuda" if use_cuda else "cpu")
 
 
-poses_imgs_path = '../video'
-
-valid_dataset = Dataset(mode='val', txt_path='network/train_val_test_splitter/val.txt', poses_path=os.path.join(poses_imgs_path, 'val'),
-                        filtered_seq_len=filtered_seq_len, filtered_seq_step_size=filtered_seq_step_size, use_optical_flow=use_optical_flow)
+valid_dataset = Dataset(mode='val', txt_path='network/train_val_test_splitter/val.txt', poses_path=os.path.join(args.poses_video_path, 'val'),
+                        filtered_seq_len=args.filtered_seq_len, filtered_seq_step_size=args.filtered_seq_step_size, use_optical_flow=args.use_optical_flow)
 valid_loader = torch.utils.data.DataLoader(valid_dataset,
-                         batch_size=batch_size,
-                         num_workers=int(workers/2),
+                         batch_size=args.batch_size,
+                         num_workers=int(args.workers/2),
                          pin_memory=True)
 
-test_dataset = Dataset(mode='test', txt_path='network/train_val_test_splitter/test.txt', poses_path=os.path.join(poses_imgs_path, 'test'),
-                       filtered_seq_len=filtered_seq_len, filtered_seq_step_size=filtered_seq_step_size, use_optical_flow=use_optical_flow)
+test_dataset = Dataset(mode='test', txt_path='network/train_val_test_splitter/test.txt', poses_path=os.path.join(args.poses_video_path, 'test'),
+                       filtered_seq_len=args.filtered_seq_len, filtered_seq_step_size=args.filtered_seq_step_size, use_optical_flow=args.use_optical_flow)
 test_loader = torch.utils.data.DataLoader(test_dataset,
-                         batch_size=batch_size,
-                         num_workers=int(workers/2),
+                         batch_size=args.batch_size,
+                         num_workers=int(args.workers/2),
                          pin_memory=True)
 
-train_dataset = Dataset(mode='train', txt_path='network/train_val_test_splitter/train.txt', poses_path=os.path.join(poses_imgs_path, 'train'),
-                        filtered_seq_len=filtered_seq_len, filtered_seq_step_size=filtered_seq_step_size, use_optical_flow=use_optical_flow)
+train_dataset = Dataset(mode='train', txt_path='network/train_val_test_splitter/train.txt', poses_path=os.path.join(args.poses_video_path, 'train'),
+                        filtered_seq_len=args.filtered_seq_len, filtered_seq_step_size=args.filtered_seq_step_size, use_optical_flow=args.use_optical_flow)
 train_loader = torch.utils.data.DataLoader(train_dataset,
-                         batch_size=batch_size,
-                         num_workers=workers,
+                         batch_size=args.batch_size,
+                         num_workers=args.workers,
                          pin_memory=True,
                          shuffle=True)
 
@@ -104,7 +112,7 @@ def train(model, criterion, optimizer, epoch, writer):
     writer.add_scalar('Precision_Avg/Train', acc_avg, epoch)
     avg_dist_arr = output_count_meter.get_distribution()
     print('====> Total train set loss: {:.4f}, acc: {:.4f}, dist: ({:.4f}, {:.4f}, {:.4f})'.format(loss_avg, acc_avg, *avg_dist_arr))
-    return loss_avg, acc_avg
+    return loss_avg, acc_avg, avg_dist_arr
 
 
 def evaluate(model, criterion, epoch, writer, loader):
@@ -139,15 +147,15 @@ def evaluate(model, criterion, epoch, writer, loader):
         writer.add_scalar('Precision_Avg/Val', acc_avg, epoch)
     avg_dist_arr = output_count_meter.get_distribution()
     print('====> Total valid set loss: {:.4f}, acc: {:.4f}, dist: ({:.4f}, {:.4f}, {:.4f})\n'.format(loss_avg, acc_avg, *avg_dist_arr))
-    return loss_avg, acc_avg
+    return loss_avg, acc_avg, avg_dist_arr
 
 
 def main():
     startTime = datetime.now()
     start_epoch = 1
 
-    model = FencingModel(use_optical_flow=use_optical_flow).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    model = FencingModel(use_optical_flow=args.use_optical_flow).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=weight_decay)
 
     if checkpoint != '':
         checkpoint_args_path = os.path.dirname(checkpoint) + '/args.pth'
@@ -171,7 +179,7 @@ def main():
     epochs_since_improvement=0
 
     # Begin!
-    for epoch in range(start_epoch, start_epoch + epochs):
+    for epoch in range(start_epoch, start_epoch + args.epochs):
 
         if adjust_lr_manually:
             # Halve learning rate if there is no improvement for 3 consecutive epochs, and terminate training after 8
@@ -180,15 +188,15 @@ def main():
             if epochs_since_improvement > 0 and epochs_since_improvement % 4 == 0:
                 adjust_learning_rate(optimizer, 0.6)
 
-        train_avg_loss, train_avg_acc = train(model, criterion, optimizer, epoch, writer)
-        val_avg_loss, val_avg_acc = evaluate(model, criterion, epoch, writer, valid_loader)
+        train_avg_loss, train_avg_acc, train_avg_dist_arr = train(model, criterion, optimizer, epoch, writer)
+        val_avg_loss, val_avg_acc, val_avg_dist_arr = evaluate(model, criterion, epoch, writer, valid_loader)
 
         if val_avg_loss < best_eval:
             torch.save(model.state_dict(), '%s/bestmodel.pth' % (expName))
             best_eval = val_avg_loss
             epochs_since_improvement = 0
-            best_val_err_full_info = {'epoch': epoch, 'train_avg_loss': train_avg_loss, 'train_avg_acc': train_avg_acc,
-                                           'val_avg_loss': val_avg_loss, 'val_avg_acc': val_avg_acc}
+            best_val_err_full_info = {'epoch': epoch, 'train_avg_loss': train_avg_loss, 'train_avg_acc': train_avg_acc, 'train_avg_dist_arr': train_avg_dist_arr,
+                                           'val_avg_loss': val_avg_loss, 'val_avg_acc': val_avg_acc, 'val_avg_dist_arr': val_avg_dist_arr}
         else:
             epochs_since_improvement += 1
             print("\nEpochs since last improvement: %d\n" % (epochs_since_improvement,))
@@ -196,16 +204,30 @@ def main():
         torch.save(model.state_dict(), '%s/lastmodel.pth' % (expName))
         torch.save([epoch, optimizer.param_groups[0]['lr']], '%s/args.pth' % (expName))
 
+    torch.save([args],'%s/args.pth' % (expName))
+
     writer.close()
     print(json.dumps(best_val_err_full_info, indent=4, sort_keys=True))
 
     print('Running Test\n')
     model.load_state_dict(torch.load('%s/bestmodel.pth' % (expName)))
-    test_avg_loss, test_avg_acc = evaluate(model, criterion, epoch, None, test_loader)
-    print('====> Total test set loss: {:.4f}, acc: {:.4f}'.format(test_avg_loss, test_avg_acc))
+    test_avg_loss, test_avg_acc, test_avg_dist_arr = evaluate(model, criterion, epoch, None, test_loader)
 
+    test_result_str = '====> Total test set loss: {:.4f}, acc: {:.4f}, dist: ({:.4f}, {:.4f}, {:.4f})\n'.format(test_avg_loss, test_avg_acc,
+                                                                                                     *test_avg_dist_arr)
+    print(test_result_str)
     print('startTime=' + str(startTime))
     print('endTime=' + str(datetime.now()))
+
+    with open('%s/results.txt' % (expName), 'w') as resfile:
+        resfile.write('Arguments:\n')
+        resfile.write(' '.join(sys.argv[1:]))
+        resfile.write('\n')
+        resfile.write('Results for exp %s:\n' % (expName))
+        json.dump(best_val_err_full_info, resfile, indent=4, sort_keys=True)
+        resfile.write('\n')
+        resfile.write(test_result_str)
+
 
 if __name__ == '__main__':
     main()
