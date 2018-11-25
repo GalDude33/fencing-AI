c058c006a6c2699d78359e3509cb861d94ba40cd
rabkinda <rabkinda@mail.tau.ac.il>
2018-11-25 16:21:06 +0200

    shuffeling change

--- network/train_val_test_splitter/DatasetSplitter.py
+++ network/train_val_test_splitter/DatasetSplitter.py
@@ -12,25 +12,25 @@ def write_list_to_file(list, filename):
         for str in list:
             f.write(str + '\n')
 
-
 videos_to_split = [os.path.splitext(ntpath.basename(vid))[0] for vid in
                    glob.glob('/media/rabkinda/DATA/fencing/FinalPoseEstimationResults/jsons*/*.json')]
 videos_to_split = [vid[:re.search("-\d{1,2}_keypoints", vid).regs[0][0]] for vid in videos_to_split]
 videos_to_split = list(set(videos_to_split))
 videos_to_split.sort()  # make sure that the filenames have a fixed order before shuffling
-random.seed(230)
-random.shuffle(videos_to_split)  # shuffles the ordering of filenames (deterministic given the chosen seed)
-
 
 def get_video_by_label(videos_to_split, label):
     return list(filter(lambda x: getLabelFromFilename(x) == label, videos_to_split))
-
 
 L_vids = get_video_by_label(videos_to_split, 'L')
 R_vids = get_video_by_label(videos_to_split, 'R')
 T_vids = get_video_by_label(videos_to_split, 'T')
 
 all_vids = [L_vids, R_vids, T_vids]
+
+for vid in all_vids:
+    random.seed(230)
+    vid.sort()
+    random.shuffle(vid)  # shuffles the ordering of filenames (deterministic given the chosen seed)
 
 train_split = 0.8
 val_split = 0.9
