dfee4331cf210909ba3a0dd51376ac05df351a19
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-11-25 14:35:55 +0200

    Fixed for balanced labels

--- network/train_val_test_splitter/DatasetSplitter.py
+++ network/train_val_test_splitter/DatasetSplitter.py
@@ -1,29 +1,46 @@
+import itertools
 import random
 import glob
 import os
 import ntpath
 import re
 
+from network.train_val_test_splitter.statistics import getLabelFromFilename
+
+
 def write_list_to_file(list, filename):
     with open(filename, 'w') as f:
         for str in list:
-            f.write(str+'\n')
+            f.write(str + '\n')
+
 
 videos_to_split = [os.path.splitext(ntpath.basename(vid))[0] for vid in
                    glob.glob('/media/rabkinda/DATA/fencing/FinalPoseEstimationResults/jsons*/*.json')]
 videos_to_split = [vid[:re.search("-\d{1,2}-\w", vid).regs[0][0]] for vid in videos_to_split]
-videos_to_split = list(set(videos_to_split))#['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+videos_to_split = list(set(videos_to_split))  # ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 videos_to_split.sort()  # make sure that the filenames have a fixed order before shuffling
 random.seed(230)
-random.shuffle(videos_to_split) # shuffles the ordering of filenames (deterministic given the chosen seed)
+random.shuffle(videos_to_split)  # shuffles the ordering of filenames (deterministic given the chosen seed)
 
-split_1 = int(0.7 * len(videos_to_split))
-split_2 = int(0.8 * len(videos_to_split))
-train_videos = videos_to_split[:split_1]
-val_videos = videos_to_split[split_1:split_2]
-test_videos = videos_to_split[split_2:]
 
-#write it to file- overwrite mode
+def get_video_by_label(videos_to_split, label):
+    return list(filter(lambda x: getLabelFromFilename(x) == label, videos_to_split))
+
+
+L_vids = get_video_by_label(videos_to_split, 'L')
+R_vids = get_video_by_label(videos_to_split, 'R')
+T_vids = get_video_by_label(videos_to_split, 'T')
+
+all_vids = [L_vids, R_vids, T_vids]
+
+train_split = 0.8
+val_split = 0.9
+
+train_videos = itertools.chain.from_iterable(vids[:int(train_split * len(vids))] for vids in all_vids)
+val_videos = itertools.chain.from_iterable(vids[int(train_split * len(vids)):int(val_split * len(vids))] for vids in all_vids)
+test_videos = itertools.chain.from_iterable(vids[int(val_split * len(vids)):] for vids in all_vids)
+
+# write it to file- overwrite mode
 write_list_to_file(train_videos, 'train.txt')
 write_list_to_file(val_videos, 'val.txt')
 write_list_to_file(test_videos, 'test.txt')
